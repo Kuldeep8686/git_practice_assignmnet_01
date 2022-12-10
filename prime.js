6f5eabb1db7e4551f7f1a44fc9e9ca85f5374265
Kuldeep Kumar Baitha <kuldeepkumar83986@gmail.com>
2022-12-10 23:35:26 +0530

    prime file added

---
+++ prime.js
@@ -0,0 +1,13 @@
+let number =17;
+let counter = 0;
+for(let i=1;i<=number;i++){
+  if(number%i==0){
+    counter++
+  } 
+   }
+if(counter==3){
+     console.log("Number is Prime");
+   }
+else{
+  console.log("Number is not a Prime");
+}
